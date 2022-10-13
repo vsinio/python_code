f4972213bbb58d8625d9d94161bd50b2372cb8e4
Ivan <markelovv1999@yandex.ru>
2022-10-13 13:13:28 +0300

    refactor code

--- hw5/task4.py
+++ hw5/task4.py
@@ -1,36 +1,34 @@
 MESSAGE = "AAAAAAAAAAAAAABBBBBCDDDDDEEEEEF"
-def RLE(somestr):  # Функция RLE RESULLT: 5A5B1C5D5E
-    final = ""
+
+def first_rle(string):  # Функция RLE RESULLT: 14A5B1C5D5E1F
+    final_str = ""
     counter = 1
-    j = 0
-    for i in somestr:
 
-        if somestr[j] == somestr[j + 1]:
-            counter += 1
-        else:
-            final += str(counter) + somestr[j]
-            counter = 1
+    for j in range(0, len(string)):
+        try:
+            if string[j] == string[j + 1]:
+                counter += 1
+            else:
+                final_str += str(counter) + string[j]
+                counter = 1
 
-        j += 1
-        if j == len(somestr) - 1:
-            final += str(counter) + somestr[j]
-            return final
+            j += 1
+        except IndexError:
+            final_str += str(counter) + string[j]
+    return final_str
 
 
-print(RLE(MESSAGE))
 
-# j+=1
-# if j+1 == len(somestr):
-# final+=str(counter)+somestr[j]
+print(first_rle(MESSAGE))
 
 MESS_RES = "14A5B1C5D5E1F"
 
 
-def res_RLE(somestr):  # decoder
+def sec_rle(string):  # decoder
     final_str = ""
     index = 0
     count = ""
-    for i in somestr:
+    for i in string:
         if i.isdigit():
             count += i
         else:
@@ -40,4 +38,4 @@ def res_RLE(somestr):  # decoder
     return final_str
 
 
-print(res_RLE(MESS_RES))
+print(sec_rle(MESS_RES))
