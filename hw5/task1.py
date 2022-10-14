a0199e73a0e88b55ee1fbc54eeba0bca44fe4fd6
Ivan <markelovv1999@yandex.ru>
2022-10-14 14:54:39 +0300

    add dec var with lc

--- hw5/task1.py
+++ hw5/task1.py
@@ -12,10 +12,11 @@
 # new_text = del_symb(text_for_def,symb)
 # print(new_text)
 
+#1st VAR
 
 TEXT = "абвесили меня сегодня абвечером абв ноабве"
 DEL_SYMB = "абв"
-
+print(TEXT.split())
 
 def delete_word(text, del_symb):
     lst = text.split()
@@ -27,3 +28,13 @@ def delete_word(text, del_symb):
     return new_lst
 
 print(delete_word(TEXT, DEL_SYMB))
+
+#2 VAR
+
+TEXT_1 = "абвесили меня сегодня абвечером абв ноабве"
+DEL_SYMB_1 = "абв"
+print(TEXT.split())
+
+lst = [el for el in TEXT_1.split() if DEL_SYMB_1 not in el]
+lst = ' '.join(lst)
+print(lst)
