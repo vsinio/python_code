85430c399f61fe0d20623b1bd14d6c0a15c6d8f3
Ivan <markelovv1999@yandex.ru>
2022-10-28 16:14:21 +0300

    made hw9

--- hw9tests/code_func.py
+++ hw9tests/code_func.py
@@ -1,4 +1,4 @@
-#1
+# 1
 def summ_digits(x):
     summ = 0
     while x != 0:
@@ -6,7 +6,8 @@ def summ_digits(x):
         x = x // 10
     return summ
 
-#2
+
+# 2
 def delete_word(text, del_symb):
     """функция принимает исходный текст и элемент для удаления, далее создает список
     в который добавляются элементы не имеющие del_symb - элементов которые удаляются"""
@@ -17,7 +18,9 @@ def delete_word(text, del_symb):
             lst_1.append(i)
     new_lst = ' '.join(lst_1)
     return new_lst
-#3 and 4
+
+
+# 3 and 4
 """алгоритм сжатия и 'разжатия'"""
 MESSAGE = "AAAAAAAAAAAAAABBBBBCDDDDDEEEEEF"
 
@@ -41,6 +44,7 @@ def first_rle(string):  # Функция RLE RESULLT: 14A5B1C5D5E1F
             final_str += str(counter) + string[index]
     return final_str
 
+
 def sec_rle(string):
     """Декодер"""
     final_str = ""
@@ -55,3 +59,44 @@ def sec_rle(string):
         index += 1
     return final_str
 
+
+# 5
+def from10to2(x):
+    two_cc = ''
+    while x > 0:
+        cc = x % 2
+        two_cc += str(cc)
+        x //= 2
+    return two_cc[::-1]
+
+#6
+def lst_keyboard(x):  # создать список от -N до N
+    len_n = -x
+    lst = []
+    for i in range(x * 2 + 1):
+        lst.append(len_n)
+        len_n += 1
+
+    return lst
+
+#7
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-2) + fibonacci(n-1)
+
+#8
+def even_fn(x):
+    if x % 2 == 0:
+        return True
+    return False
+#9 odd
+def odd(x):
+    if x % 2 != 0:
+        return True
+    return False
+#sec var fibbo
+def secfibonacci(n): #фибоначчи второй вариант
+    if n < 2:
+        return n
+    return secfibonacci(n-2) + secfibonacci(n-1)
--- hw9tests/test1.py
+++ hw9tests/test1.py
@@ -3,8 +3,16 @@ from code_func import summ_digits
 from code_func import delete_word
 from code_func import first_rle
 from code_func import sec_rle
+from code_func import from10to2
+from code_func import lst_keyboard
+from code_func import fibonacci
+from code_func import even_fn
+from code_func import odd
+from code_func import secfibonacci
+
 
 class TestFunc(unittest.TestCase):
+    """класс тестов номер 1"""
     def test_summ(self):
         self.assertEqual(summ_digits(123), 6)
 
@@ -12,8 +20,28 @@ class TestFunc(unittest.TestCase):
         self.assertEqual(delete_word("абвесили меня сегодня абвечером абв ноабве", "абв"), "меня сегодня")
 
     def test_coder(self):
-        self.assertEqual(first_rle("AAAAAAAAAAAAAABBBBBCDDDDDEEEEEF"),"14A5B1C5D5E1F")
+        self.assertEqual(first_rle("AAAAAAAAAAAAAABBBBBCDDDDDEEEEEF"), "14A5B1C5D5E1F")
 
     def test_decoder(self):
-        self.assertEqual(sec_rle("14A5B1C5D5E1F"),"AAAAAAAAAAAAAABBBBBCDDDDDEEEEEF")
+        self.assertEqual(sec_rle("14A5B1C5D5E1F"), "AAAAAAAAAAAAAABBBBBCDDDDDEEEEEF")
 
+    def test_cc(self):
+        self.assertEqual(from10to2(10), "1010")
+
+
+class TestFunc2(unittest.TestCase):
+    """класс тестов номер 2"""
+    def test_lst(self):
+        self.assertEqual(lst_keyboard(2), [-2, -1, 0, 1, 2])
+
+    def test_fibo(self):
+        self.assertEqual(fibonacci(2), 1)
+
+    def test_even_dig(self):
+        self.assertTrue(even_fn(6), True)
+
+    def test_odd_dig(self):
+        self.assertFalse(odd(6),True)
+
+    def test_fibo_2(self):
+        self.assertEqual(fibonacci(5),secfibonacci(5))
